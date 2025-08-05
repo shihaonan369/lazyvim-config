2e592055d90f2424d9780b5c4c137544e6b4613c
shihaonan <shihaonan369@live.com>
2025-08-05 22:26:50 +0800

    feat: 更改默认shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  vim.opt.shell = "pwsh -NoLogo -ExecutionPolicy RemoteSigned"
+  vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+else
+  vim.opt.shell = "zsh"
+end
