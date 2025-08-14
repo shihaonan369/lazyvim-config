94c02e0118c7e9728bc6dac6d2d23b3edaf61ef3
shihaonan <shihaonan369@live.com>
2025-08-14 09:08:45 +0800

    feat: change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,12 +13,22 @@ return {
       transparent = true,
     },
   },
+  {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
+    },
+  },
 
   -- Configure LazyVim to load theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      colorscheme = "catppuccin",
     },
   },
 }
