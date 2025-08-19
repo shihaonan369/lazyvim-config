695df2333223c4832cde1e87a61e760d864d527a
shihaonan <shihaonan369@live.com>
2025-08-19 20:40:44 +0800

    feat: 添加python venv选择插件

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -24,11 +24,27 @@ return {
     },
   },
 
-  -- Configure LazyVim to load theme
+  -- Configure LazyVim to load theme based on TERMINAL_THEME
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    opts = function()
+      -- 读取环境变量，不区分大小写
+      local term_theme = os.getenv("TERMINAL_THEME")
+      if term_theme then
+        term_theme = term_theme:lower()
+      end
+
+      local theme_map = {
+        retro = "cyberdream",
+        paper = "catppuccin",
+      }
+
+      -- 默认使用 catppuccin
+      local chosen_theme = theme_map[term_theme] or "catppuccin"
+
+      return {
+        colorscheme = chosen_theme,
+      }
+    end,
   },
 }
