346312188ff294701cc1bd42d6a0bdef4c79d281
shihaonan <shihaonan369@live.com>
2026-01-05 17:29:53 +0800

    feat: container支持复制

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,27 @@ if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
 else
   vim.opt.shell = "zsh"
 end
+
+if vim.env.SSH_CONNECTION or vim.env.DOCKER_EXEC then
+  local opts = { noremap = true, silent = true }
+
+  vim.opt.clipboard = (vim.env.SSH_CONNECTION or vim.env.DOCKER_EXEC) and "" or "unnamedplus"
+  -- 普通模式复制当前行到 "+ 寄存器
+  vim.api.nvim_set_keymap("n", "yy", '"+yy', opts)
+  vim.api.nvim_set_keymap("n", "Y", '"+yy', opts)
+
+  -- 可视模式复制选中内容到 "+ 寄存器
+  vim.api.nvim_set_keymap("v", "y", '"+y', opts)
+
+  -- 剪切（可选）
+  vim.api.nvim_set_keymap("n", "dd", '"+dd', opts)
+  vim.api.nvim_set_keymap("v", "d", '"+d', opts)
+
+  -- 普通模式粘贴
+  vim.api.nvim_set_keymap("n", "p", '"+p', opts)
+  vim.api.nvim_set_keymap("n", "P", '"+P', opts)
+
+  -- 可视模式粘贴
+  vim.api.nvim_set_keymap("v", "p", '"+p', opts)
+  vim.api.nvim_set_keymap("v", "P", '"+P', opts)
+end
