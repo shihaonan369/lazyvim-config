2b043cbe924d57aaefe5b628f0ccfbaadd91343d
shihaonan <shihaonan369@live.com>
2026-04-07 18:58:22 +0800

    Revert "refactor: terminal模式禁用鼠标"
    This reverts commit 152c702edab9212da8fb21e483b383b23d6a8e08.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,8 +14,3 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
-vim.api.nvim_create_autocmd("TermOpen", {
-  callback = function()
-    vim.opt_local.mouse = ""
-  end,
-})
