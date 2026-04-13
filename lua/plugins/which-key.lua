8f5e806ad2a6e76218cc5bf1b46cbd095e9292bd
shihaonan <shihaonan369@live.com>
2026-04-13 22:30:27 +0800

    style: 给ai和terminal菜单添加图标和说明

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>a", group = "ai", icon = " " },
+        { "<leader>t", group = "terminal", icon = "" },
+      },
+    },
+  },
+}
