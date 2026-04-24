ca2413b69086b4e8f582f169b82857646fb7ff37
shihaonan <shihaonan369@live.com>
2026-04-24 23:04:58 +0800

    refactor: java默认禁用自动格式化

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,7 +9,7 @@
 
 -- Disable autoformat for lua files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "python" },
+  pattern = { "python", "java" },
   callback = function()
     vim.b.autoformat = false
   end,
